45f37b00edbc47ef219399026323ae341178558c
Prune Pillone <prune6@gmail.com>
2018-06-19 22:26:59 +0200

    Now getting the total number of followers with Twitter API

--- src/main/java/com/les_napoleons/StatsUtils.java
+++ src/main/java/com/les_napoleons/StatsUtils.java
@@ -1,32 +1,49 @@
 package com.les_napoleons;
 
-import twitter4j.*;
+import twitter4j.IDs;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StatsUtils {
-
 
     public void getTwitterFollowers() {
 
         TwitterFactory factory = new TwitterFactory();
         Twitter twitter = factory.getInstance();
-
         twitter.setOAuthConsumer("6WcdrVzkqTuDvEZMQeLYNnK48", "aBt2toj0sXiARBht5wh65ddeJ2ndrrSw8Pl87zUsl5iFac2Y25");
         AccessToken accessToken = new AccessToken("2467520784-CHESBQLEo2rKXm1d2K2wdPrPs1XeXhAMMJK5Gps", "kFHXnwmjYIAyylI4qKt1m8ke2iGOKAZW99UKIWtFe7CVi");
         twitter.setOAuthAccessToken(accessToken);
-
+        //TODO https://twittercounter.com/pages/api
+        //TODO Request API key data in JSON, parse JSON etc
         String twitterScreenName;
         try {
-            twitterScreenName = twitter.getScreenName();
+            List<Long> followerList = new ArrayList<Long>();
+            long cursor = -1L;
+            IDs ids;
+            do {
+                ids = twitter.getFollowersIDs(cursor);
+                for (long userID : ids.getIDs()) {
+                    followerList.add(userID);
+                }
+            } while ((cursor = ids.getNextCursor()) != 0);
+            //getFollowersIds only returns 5k ids, so a cursor is needed to run through all the list
+            System.out.println(followerList.size());
 
-            IDs followerIDs = twitter.getFollowersIDs(twitterScreenName, -1);
+            /*IDs followerIDs = twitter.getFollowersIDs(twitterScreenName, -1);
             long[] ids = followerIDs.getIDs();
+            IDs id = twitter.getFriendsIDs(20000);
 
-            System.out.println(twitter.showUser(ids[159]).getScreenName());
-            System.out.println(twitter.showUser(ids[159]).getLocation());
-            System.out.println(twitter);
+            IDs test = twitter.getFollowersIDs(twitterScreenName,);
+            System.out.println(test.getIDs().length);
+
+            //System.out.println(twitter.showUser(ids[159]).getScreenName());
+            //System.out.println(twitter.showUser(ids[159]).getLocation());
+            //System.out.println(twitter);*/
 
             /*for (long id : ids) {
                 twitter4j.User user = twitter.showUser(id);
